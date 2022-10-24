b02dc1295d0e2414d736300ebbfc7cd6b48f87da
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2022-10-24 18:15:50 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-sudo /bin/apt update && sudo /bin/apt upgrade -y && sudo /bin/apt install curl git net-tools neofetch ufw -y && /bin/curl -sSL https://install.pi-hole.net | bash && sudo -E /bin/bash -c "$(curl -s https://raw.githubusercontent.com/mysteriumnetwork/node/master/install.sh)"
+sudo /bin/apt update && 
+sudo /bin/apt upgrade -y &&
+# INSTALL PACKAGES FROM RASPBIAN REP
+sudo /bin/apt install curl git net-tools neofetch ufw -y && 
+# INSTALL OTHER PACKAGES
+sudo /bin/curl -sSL https://install.pi-hole.net | bash && 
+sudo -E /bin/bash -c "$(curl -s https://raw.githubusercontent.com/mysteriumnetwork/node/master/install.sh)"
+# SYSTEM CONFIGURATION
