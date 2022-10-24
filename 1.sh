973ef701a2aa1ea1df2c0e0707f080d22006e952
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2022-10-24 13:51:09 +0100

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo /bin/apt update && sudo /bin/apt upgrade -y && sudo /bin/apt install curl git net-tools neofetch ufw && /bin/curl -sSL https://install.pi-hole.net | bash && sudo -E /bin/bash -c "$(curl -s https://raw.githubusercontent.com/mysteriumnetwork/node/master/install.sh)"
