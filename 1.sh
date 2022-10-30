6ce80bfd56ab0b484b2eace047630d668d95f848
Pendrag00n <88731722+Pendrag00n@users.noreply.github.com>
2022-10-30 02:32:47 +0000

    fized minnor problems

--- 1.sh
+++ 1.sh
@@ -13,7 +13,8 @@ sudo /usr/bin/apt install curl git net-tools neofetch ufw fail2ban -y &&
 sudo /usr/bin/curl -sSL https://install.pi-hole.net | /usr/bin/bash && 
 sudo -E /usr/bin/bash -c "$(curl -s https://raw.githubusercontent.com/mysteriumnetwork/node/master/install.sh)" && 
 sudo /usr/bin/curl -L https://install.pivpn.io | /usr/bin/bash && 
-sudo /usr/bin/curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | /usr/bin/bash && sudo /usr/bin/install filebrowser /usr/local/bin/ && 
+sudo /usr/bin/curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | /usr/bin/bash && 
+# sudo /usr/bin/install filebrowser /usr/local/bin/ && 
 #
 # SYSTEM CONFIGURATION
 #
@@ -24,9 +25,9 @@ sudo /usr/bin/systemctl stop pihole-FTL.service &&
 #
 # USER CREATION AND PERMISSION GRANTING
 #
-sudo /usr/sbin/adduser --system --group --HOME /var/lib/filebrowser/ --shell /usr/sbin/nologin --no-create-home filebrowser && 
+sudo /usr/bin/mkdir /var/lib/filebrowser && sudo /usr/sbin/adduser --system --group --HOME /var/lib/filebrowser/ --shell /usr/sbin/nologin --no-create-home filebrowser && 
 sudo /usr/sbin/addgroup -S filebrowser && 
-sudo /usr/sbin/adduser -S -s /sbin/nologin -H -h /var/lib/filebrowser -G filebrowser filebrowser && 
+sudo /usr/sbin/adduser --system /sbin/nologin -H -h /var/lib/filebrowser -G filebrowser filebrowser && 
 sudo /usr/bin/chown -Rc filebrowser:filebrowser /var/lib/filebrowser && 
 sudo /usr/bin/touch /var/log/filebrowser.log && sudo /usr/bin/chown -c filebrowser:filebrowser /var/log/filebrowser.log && 
 #
@@ -41,6 +42,6 @@ sudo /usr/bin/cp .filebrowser.toml /etc/filebrowser/ &&
 #
 # ENABLE AND START SERVICES
 #
-sudo /usr/bin/systemctl edit --force --full filebrowser && 
+# sudo /usr/bin/systemctl edit --force --full filebrowser && 
 sudo /usr/bin/systemctl enable mysterium-node.service && sudo /usr/bin/systemctl start mysterium-node.service &&
 sudo /usr/bin/systemctl enable stop pihole-FTL.service && sudo /usr/bin/systemctl start stop pihole-FTL.service &&
